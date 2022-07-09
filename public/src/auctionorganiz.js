35ecddd77d619915ad3bf8d08effbccb49d82c75
Thidawan <aimaim.3112@gmail.com>
2022-07-09 14:23:51 +0700

    commit 1

---
+++ public/src/auctionorganiz.js
@@ -0,0 +1,18 @@
+$(document).$(document).ready(function () {
+    const rawdata = [
+        { "order": "1","itemimg":"","lastprice":"10 บาท","bidder":"deo up1","timer":"22 mins.", "projectname": "กระเป๋าเป้สีเหลือง", "ownermae": "กันทิตา บุญรักษานะ" },
+        { "order": "2","itemimg":"","lastprice":"10 บาท","bidder":"deo up1","timer":"22 mins.", "projectname": "รถยนต์สีแดง", "ownermae": "นิชา อรุณรักษา"},
+        { "order": "3","itemimg":"","lastprice":"10 บาท","bidder":"deo up1","timer":"22 mins.", "projectname": "จอมินิเตอร์ ขนาด 32 นิ้ว", "ownermae": "ลักขณา รักนะจุ๊บจุ๊บ" },
+        { "order": "4","itemimg":"","lastprice":"10 บาท","bidder":"deo up1","timer":"22 mins.", "projectname": "รักน้ำ รักป่า รักน้องช้างด้วยน๊าา", "ownermae": "รุ่งนภา คำแพง" },
+        { "order": "5","itemimg":"","lastprice":"10 บาท","bidder":"deo up1","timer":"22 mins.", "projectname": "คุณปู่ คุณย่า เหง๊าเหงา มาหาเค้าหน่อย", "ownermae": "ก้อง บ่มีอิหยังมาพังทลาย" },
+        { "order": "6","itemimg":"","lastprice":"10 บาท","bidder":"deo up1","timer":"22 mins.", "projectname": "พรุ่งนี้จะรักกันก็ยังไม่สาย แต่ถ้าน้องแมวไม่มีข้าวกินน้องจะไม่รอด", "ownermae": "รักเธอ แล้วใจก็มีเสียงเพลง" },
+        { "order": "7","itemimg":"","lastprice":"10 บาท","bidder":"deo up1","timer":"22 mins.", "projectname": "รักกันอย่าบังคับ อย่าบังคับ พรุ่งนี้นะครับมาช่วยเค้าหน่อย", "ownermae": "รักสวย รักงานเป็นจิตใจ"},
+        { "order": "8","itemimg":"","lastprice":"10 บาท","bidder":"deo up1","timer":"22 mins.", "projectname": "แมว หมา อาหาร น้องต้องการความช่วยเหลือ", "ownermae": "อย่าให้ พรุ่งนี้มาทำร้ายเรา" },
+    ];
+
+    $('#btnallauction').click(function(){
+        $('#bodyauction').DataTable({
+            
+        })
+    })
+}); 
